a8c5dc9cc6eed036043d9bf29010836106830bf5
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-02-12 13:46:38 +0200

    lämpötilanmuunninta

--- lampotila.js
+++ lampotila.js
@@ -1,9 +1,40 @@
-let kayttajanSyote = document.getElementById('syote');
-let tulos = document.getElementById('muunnos');
-const klikki = document.getElementById('klik');
+document.getElementById('muunna').addEventListener('click', function(event) {
+    event.preventDefault();
+    // tallennetaan käyttäjän syöte muuttujaksi
+    let syote = document.getElementById('asteet');
+    // putsataan syote mahdollisten virheiden varalta
+    let syoteArvo = syote.value.trim();
 
-function naytaMuunnos(){
-    tulos.innerHTML = kayttajanSyote.value;
-}
+    // tarkistetaan että syöte ei ole tyhjä tai ei-numeraalinen
+    if (syoteArvo === '' || isNaN(syoteArvo)) {
+        alert('Syötä numero!');
+        // jos virheellinen syöte, nollataan input-kenttä
+        syote.value = '';
+        return;
+    }
 
-klikki.addEventListener('click', naytaMuunnos);
+    // Muunnetaan syöte numeroksi
+    let syotettyLuku = parseFloat(syoteArvo);
+
+    // alustetaan muunnettu luku
+    let muunnettuLuku;
+    // määritellään mihin muutetaan
+    if (document.getElementById('valitse').value === 'celsius') {
+        muunnettuLuku = (syotettyLuku * 1.8) + 32;
+    } else {
+        muunnettuLuku = (syotettyLuku - 32) * 1.8; 
+    }
+    
+    // tähän vielä desimaalille vaihtoehdot
+    let valittuDesimaali = 3;
+
+    if (muunnettuLuku > -273.15){
+
+        document.getElementById('tulos').innerHTML = muunnettuLuku.toFixed(valittuDesimaali);
+    }
+    // tähän joku muutos vielä
+    else{
+        document.getElementById('tulos').innerHTML = muunnettuLuku.toFixed(valittuDesimaali);
+        document.getElementById('abso').innerHTML = ('Kylmempi kuin absoluuttinen nollapiste (-273,15)!');
+    }
+});
