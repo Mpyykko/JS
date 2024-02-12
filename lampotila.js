4bd02d23b11f76ed523a0e5a932c97e2792d9bfb
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-02-12 08:40:59 +0200

    lisätty git ignore

--- lampotila.js
+++ lampotila.js
@@ -1,1 +1,9 @@
-let syote 
+let kayttajanSyote = document.getElementById('syote');
+let tulos = document.getElementById('muunnos');
+const klikki = document.getElementById('klik');
+
+function naytaMuunnos(){
+    tulos.innerHTML = kayttajanSyote.value;
+}
+
+klikki.addEventListener('click', naytaMuunnos);
