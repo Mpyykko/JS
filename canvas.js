cd376aa070d5b442a4b03d8c5ceb35d28ecba646
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-02-14 12:33:47 +0200

    mobiilia taas

--- canvas.js
+++ canvas.js
@@ -3,11 +3,14 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let c = canvas.getContext('2d');
+let c1 = canvas.getContext('2d');
+let c2 = canvas.getContext('2d');
+let c3 = canvas.getContext('2d');
+let c4 = canvas.getContext('2d');
 
 //c.fillRect(x, y, width, height);
 
-c.fillRect(100, 100, 100, 100);
-c.fillRect(400, 100, 100, 100);
-c.fillRect(100, 500, 100, 100);
-c.fillRect(600, 100, 100, 100);
+c1.fillRect(100, 100, 100, 100);
+c2.fillRect(400, 100, 50, 50);
+c3.fillRect(100, 500, 100, 100);
+c4.fillRect(600, 100, 100, 100);
