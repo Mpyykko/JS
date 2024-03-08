eb01e43bf6b6c065a090abf6b0834fe56a438ee7
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-03-08 11:19:16 +0200

    clickeriä, hedelmäpelin mobiilia ym.

--- clicker.js
+++ clicker.js
@@ -1,14 +1,16 @@
 const klikki = document.getElementById('cookie');
 const displayCookies = document.getElementById('displayCookies');
 const uusiPeli = document.getElementById('uusiPeli');
-const upgrade = document.getElementById('upgrade');
+const tuplaklikki = document.getElementById('tuplaklikki');
 const tuoppi = document.getElementById('naytaTuopit');
 const promille = document.getElementById('naytaPromillet');
+const promillejapois = document.getElementById('promillejapois');
 
 
 klikki.addEventListener('click', cookieClicked);
 uusiPeli.addEventListener('click', aloitaUusiPeli);
-upgrade.addEventListener('click', upgradeClicked);
+tuplaklikki.addEventListener('click', tuplanopeusKlikattu);
+promillejapois.addEventListener('click', promilleKlikattu);
 
 let klikit = 40;
 let multiplier = 1;
@@ -20,7 +22,9 @@ let promillet = 0.00;
 function cookieClicked() {
     if (klikit >= 1) {
         klikit = klikit - multiplier;
-    } 
+    }
+
+    
     ilmoitusteksti.innerHTML = ('');
 
     if (klikit <= 40 && klikit > 30) {
@@ -51,6 +55,9 @@ function cookieClicked() {
     
     displayCookiesAmount();
 
+
+ 
+
     if(promillet === 0.25){
         level1();
     }
@@ -63,6 +70,13 @@ function cookieClicked() {
     else if(promillet >= 1.5){
         level4();
     }
+
+    if(tuplanopeus)
+    {
+        klikit -= 1;
+    }
+
+    
 }
 
 
@@ -91,29 +105,52 @@ function aloitaUusiPeli() {
     document.getElementById('cookie').src = 'clicker-kuvat/tuoppitaysi2.png';
     Tuopit();
     Promillet();
+    tuplanopeus = false;
 
 }
 
+// promilleja pois-toiminto
+
+function promilleKlikattu(){
+
+    if(tuopit <1){
+        ilmoitusteksti.innerHTML = ('Ei tarpeeksi rahaa!');
+    }
+
+    else if (promillet >= 0.25){
+        tuopit --;
+        promillet -= 0.25;
+        Promillet();
+        Tuopit();
+    }
+
+    else {
+        ilmoitusteksti.innerHTML = ('Computer says no');
+    }
+}
+
+
 // Tähän tuplanopeus
 
+let tuplanopeus = false;
 
     
-   
-
-
-function upgradeClicked() {
+function tuplanopeusKlikattu() {
     if (tuopit >= multiplierCost) {
         tuopit = tuopit - multiplierCost;
         Tuopit();
         displayCookiesAmount();
         ilmoitusteksti.innerHTML = ('Ostettu tuplanopeus!');
+        tuplanopeus = true;
 
-        klikit = klikit - 1;
+        
 
 
     } else {
         ilmoitusteksti.innerHTML = ('Ei tarpeeksi kolikoita!');
     }
+
+   
 }
 
 // Vaikeustasoja
@@ -139,7 +176,7 @@ function gameOver(){
         klikki.removeEventListener('click', cookieClicked);
         ilmoitusteksti.innerHTML = ('Pääsit pelin läpi. Olet melkoinen juoppo!');
         document.getElementById('cookie').classList.remove('animated1','animated2','animated3','animated4');
-
+        tuplanopeus = false;
     }
 }
 
