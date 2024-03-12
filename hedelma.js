9ce732125528fd81763893689cb361b4b62f7980
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-03-12 12:35:17 +0200

    hedelmää

--- hedelma.js
+++ hedelma.js
@@ -1,2 +1,64 @@
 // äänikomennot
 
+// pelaa-painike
+const pelaa = document.getElementById('pelaa-painike');
+pelaa.addEventListener('click', pelikierros);
+
+// muuttujat saldolle, panokselle ja voitoille
+let saldo = 100;
+const naytaUusisaldo = document.getElementById('saldo');
+let panos = 1;
+const naytaUusipanos = document.getElementById('panos');
+
+
+// panos pienenee
+
+const panosPienemmalle = document.getElementById('panosAlas-painike');
+panosPienemmalle.addEventListener('click', vahennaPanosta);
+
+// panos suurenee
+
+const panosSuuremmalle = document.getElementById('panosYlos-painike');
+panosSuuremmalle.addEventListener('click', suurennaPanosta);
+
+// pääohjelma
+function pelikierros(){
+    if(saldo > 0 && panos <= saldo ){
+        saldo = saldo - panos;
+        naytaSaldo();
+    }
+
+   
+
+
+}
+
+// pääohjelma loppuu
+
+
+
+function rullatPyorii(){
+   
+}
+// saldon päivitys-funktio
+function naytaSaldo() {
+    naytaUusisaldo.innerHTML = (`Saldo ${saldo} €`);
+}
+
+// panoksen päivitys-funktio
+function vahennaPanosta() {
+    if(panos >1){
+    panos--;
+    naytaUusipanos.innerHTML = (`Panos ${panos} €`);
+    }
+}
+
+// panoksen päivitys-funktio
+function suurennaPanosta() {
+    if(panos <10 ){
+    panos++;
+    naytaUusipanos.innerHTML = (`Panos ${panos} €`);
+    }
+}
+
+
